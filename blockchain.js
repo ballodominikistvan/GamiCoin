946a03066339c445ae65c0c4f7a8787f22f51e26
Bálló Dominik István <ballodominikistvan@gmail.com>
2023-10-24 17:48:54 +0200

    The blockchain datastructure finished.

--- blockchain.js
+++ blockchain.js
@@ -3,6 +3,7 @@ const sha256 = require('sha256');
 function Blockchain(){
     this.chain = [];
     this.pendingTransactions = [];
+    this.createNewBlock(1998, '0','0');
 }
 
 Blockchain.prototype.createNewBlock = function(nonce, previousBlockHash, hash){
