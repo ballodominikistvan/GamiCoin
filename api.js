0bdedada6428f0c3ae08597ab2c07ccdf5af5225
Bálló Dominik István <ballodominikistvan@gmail.com>
2023-10-26 13:31:19 +0200

    blockchain endpoint implemented

--- api.js
+++ api.js
@@ -1,16 +1,20 @@
 const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
+const Blockchain = require('./blockchain');
+
+const GamiCoin = new Blockchain();
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}));
 
 app.get('/blockchain', function(req, res){
-
+    res.send(GamiCoin);
 });
 
 app.post('/transaction', function(req, res){
-
+    console.log(req.body);
+    res.send('The amount of transaction is '+ req.body.amount+' GamiCoin!')
 });
 
 app.get('/mine', function(req, res){
